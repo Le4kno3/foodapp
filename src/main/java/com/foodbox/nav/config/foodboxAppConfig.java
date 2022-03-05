4eee4b9c6b43a839b1ccfd64d78bf682ae32728b
Takshil Patil <jlelo403@gmail.com>
2022-03-05 05:55:43 +0530

    Update foodboxAppConfig.java

--- src/main/java/com/foodbox/nav/config/foodboxAppConfig.java
+++ src/main/java/com/foodbox/nav/config/foodboxAppConfig.java
@@ -66,7 +66,7 @@ public class foodboxAppConfig implements WebMvcConfigurer{
 		//set the db info to the datasource object
 		dataSource.setUsername("root");
 		dataSource.setPassword("12Password12#");
-		dataSource.setUrl("jdbc:mysql://172.17.0.1:3306/cart_db?useSSL=false");
+		dataSource.setUrl("jdbc:mysql://mysqldb-container:3306/cart_db?useSSL=false");
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		
 		return dataSource;
